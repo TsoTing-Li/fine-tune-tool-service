ca4f44899d2f8f5b50dc6d54f638a0ff258293de
Steven <tsoting_li@innodisk.com>
2024-11-29 18:10:28 +0800

    chore: remove not necessary function

--- inno_service/routers/data/validator.py
+++ inno_service/routers/data/validator.py
@@ -5,22 +5,6 @@ from pydantic import BaseModel, model_validator
 from inno_service.utils.error import ResponseErrorHandler
 
 
-class MaxBodySizeException(Exception):
-    def __init__(self, body_len: str):
-        self.body_len = body_len
-
-
-class MaxbodySizeValidator:
-    def __init__(self, max_size: int):
-        self.body_len = 0
-        self.max_size = max_size
-
-    def __call__(self, chunk: bytes):
-        self.body_len += len(chunk)
-        if self.body_len > self.max_size:
-            raise MaxBodySizeException(body_len=self.body_len)
-
-
 class PostData(BaseModel):
     dataset_file_name: str
     dataset_file: UploadFile
