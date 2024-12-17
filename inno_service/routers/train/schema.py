19ba797f33cbb231c3e362e54c4580c20570f135
Steven <tsoting_li@innodisk.com>
2024-12-17 18:03:44 +0800

    fix: update regex to correctly match docker container name

--- inno_service/routers/train/schema.py
+++ inno_service/routers/train/schema.py
@@ -161,7 +161,7 @@ class PostStartTrain(BaseModel):
 
         if (
             self.train_name
-            and bool(re.search(r"[^a-zA-Z0-9_\-\s]+", self.train_name)) is True
+            and not re.fullmatch(r"[a-zA-Z0-9][a-zA-Z0-9_.-]+", self.train_name)
         ):
             error_handler.add(
                 type=error_handler.ERR_VALIDATE,
@@ -184,7 +184,7 @@ class PostStopTrain(BaseModel):
 
         if (
             self.train_name
-            and bool(re.search(r"[^a-zA-Z0-9_\-\s]+", self.train_name)) is True
+            and not re.fullmatch(r"[a-zA-Z0-9][a-zA-Z0-9_.-]+", self.train_name)
         ):
             error_handler.add(
                 type=error_handler.ERR_VALIDATE,
