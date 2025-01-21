68a2f19a51e70a9ab0cb75c795b61e8988165a88
Steven <tsoting_li@innodisk.com>
2025-01-21 17:41:40 +0800

    feat: add return unix format time

--- inno_service/utils/utils.py
+++ inno_service/utils/utils.py
@@ -1,5 +1,7 @@
+import time
 import uuid
 from datetime import datetime
+from typing import Union
 
 
 def generate_uuid() -> str:
@@ -11,6 +13,9 @@ def generate_uuid() -> str:
     return str(uuid.uuid4())
 
 
-def get_current_time() -> str:
-    now = datetime.now()
-    return now.strftime("%Y-%m-%d-%H-%M-%S")
+def get_current_time(use_unix: bool) -> Union[int, str]:
+    if use_unix:
+        return int(time.time())
+    else:
+        now = datetime.now()
+        return now.strftime("%Y-%m-%d-%H-%M-%S")
