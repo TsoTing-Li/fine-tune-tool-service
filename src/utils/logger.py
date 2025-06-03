cbc150bdfb78911d1321086807ebd97e04694150
Steven <tsoting_li@innodisk.com>
2025-06-03 17:12:56 +0800

    feat: optimize logger process

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -11,30 +11,37 @@ def setup_logger(folder: str, name: str, limit: int, count: int):
     # Get uvicorn logger
     logger = logging.getLogger(BASE_LOGGER)
 
-    # Create Logs Folder
-    folder = Path(folder)
-    if not folder.exists():
-        folder.mkdir(exist_ok=True, parents=True)
-
-    # Combine Path
-    file_handler = RotatingFileHandler(
-        folder / name,
-        mode="a",
-        encoding="utf-8",
-        maxBytes=int(limit),
-        backupCount=int(count),
-        delay=0,
-    )
-
     # Define Formatter
     formatter = logging.Formatter(
-        "%(asctime)s [%(levelname)-.4s] %(message)s (%(pathname)s:%(lineno)s)",
+        "%(asctime)s [%(levelname)-.4s] %(message)s (%(filename)s:%(lineno)s)",
         "%y-%m-%d %H:%M:%S",
     )
 
-    file_handler.setFormatter(formatter)
-    file_handler.setLevel(logging.ERROR)
-    logger.addHandler(file_handler)
+    if folder:
+        # Create Logs Folder
+        sub_folder = Path(folder) / name
+        sub_folder.mkdir(exist_ok=True, parents=True)
+
+        # Combine Path
+        file_handler = RotatingFileHandler(
+            sub_folder / f"{name}.log",
+            mode="a",
+            encoding="utf-8",
+            maxBytes=int(limit),
+            backupCount=int(count),
+            delay=0,
+        )
+
+        file_handler.setFormatter(formatter)
+        file_handler.setLevel(logging.ERROR)
+        logger.addHandler(file_handler)
+
+    else:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        stream_handler.setLevel(logging.ERROR)
+        logger.addHandler(stream_handler)
+
     return logger
 
 
