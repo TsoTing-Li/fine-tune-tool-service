68670b3f9d1ac76dbe5c27da0a073087b15eca2f
Steven <tsoting_li@innodisk.com>
2025-06-09 16:38:19 +0800

    chore: optimize generate .env file process

--- generate_env.py
+++ generate_env.py
@@ -1,7 +1,14 @@
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
 
-import tomllib
+try:
+    import tomllib
+
+    _is_stdlib = True
+except ModuleNotFoundError:
+    import toml as tomllib
+
+    _is_stdlib = False
 
 
 def build_parse() -> ArgumentParser:
@@ -15,37 +22,81 @@ def build_parse() -> ArgumentParser:
 def main(args: Namespace):
     cwd = Path.cwd()
     pyproject_file = cwd / "pyproject.toml"
+    version_file = cwd / "version.toml"
     env_temp = cwd / ".env.template"
     env_file = cwd / ".env"
 
     if not pyproject_file.exists():
         raise FileNotFoundError("pyproject.toml not found")
 
+    if not version_file.exists():
+        raise FileNotFoundError("version.toml not found")
+
     if not env_temp.exists():
         raise FileNotFoundError(".env.template not found")
 
-    with pyproject_file.open("rb") as f:
-        pyproject = tomllib.load(f)
+    mode = "rb" if _is_stdlib else "r"
+    with pyproject_file.open(mode) as f:
+        pyproject_file_content = tomllib.load(f)
 
-    version = pyproject["project"]["version"]
+    project_version = pyproject_file_content["project"]["version"]
+
+    with version_file.open(mode) as f:
+        version_file_content = tomllib.load(f)
+
+    assert project_version == version_file_content["project"]["version"], (
+        "project version does not match env version"
+    )
+
+    placeholders = {
+        "project_name": version_file_content["project"]["name"].lower(),
+        "docker_username": version_file_content["docker"]["info"]["username"],
+        "docker_base_repo": version_file_content["docker"]["info"]["base_repo"],
+        "redis_username": args.redis_username,
+        "redis_password": args.redis_password,
+        "mount_path": args.mount_path,
+    }
+
+    for service_key, service_value in version_file_content["services"].items():
+        placeholders[f"{service_key.replace('-', '_')}_name"] = service_value["name"]
+        placeholders[f"{service_key.replace('-', '_')}_version"] = service_value[
+            "version"
+        ]
+
+    for service_key, service_value in version_file_content["shared"].items():
+        placeholders[f"{service_key.replace('-', '_')}_name"] = service_value["name"]
+        placeholders[f"{service_key.replace('-', '_')}_version"] = service_value[
+            "version"
+        ]
+
+    for thirdparty_key, thirdparty_value in version_file_content["thirdparty"].items():
+        placeholders[f"{thirdparty_key.replace('-', '_')}_name"] = thirdparty_value[
+            "name"
+        ]
+        placeholders[f"{thirdparty_key.replace('-', '_')}_version"] = thirdparty_value[
+            "version"
+        ]
 
     with env_temp.open() as f:
         env_temp_content = f.read()
-        env_temp_content = env_temp_content.replace("{project_version}", version)
-        env_temp_content = env_temp_content.replace(
-            "{redis_username}", args.redis_username
-        )
-        env_temp_content = env_temp_content.replace(
-            "{redis_password}", args.redis_password
-        )
-        env_temp_content = env_temp_content.replace("{mount_path}", args.mount_path)
+
+        for key, value in placeholders.items():
+            env_temp_content = env_temp_content.replace(f"{{{key}}}", value)
 
     with env_file.open("w") as f:
         f.write(env_temp_content)
 
-    print(f"Generated .env file with VERSION={version}")
+    print(f"Generated .env file with VERSION={project_version}")
 
 
 if __name__ == "__main__":
     args = build_parse().parse_args()
-    main(args=args)
+
+    try:
+        main(args=args)
+    except FileNotFoundError as e:
+        print(f"{e}")
+    except AssertionError as e:
+        print(f"{e}")
+    except Exception as e:
+        print(f"{e}")
